4c97f9a6db84c69aff4f2e29d85afe96bec459c7
Jack Maney <jack@jackmaney.com>
2014-02-01 01:20:08 -0600

    Guestimated collision rectangles within the animation frames

--- Game/Shared/GameConstants.py
+++ Game/Shared/GameConstants.py
@@ -9,7 +9,21 @@ MAX_FPS = 60
 MOLARR_SWINGING_IMGS = []
 
 for i in list(range(30, 101, 10)):
-    MOLARR_SWINGING_IMGS.append(os.path.join("Game", "Assets", "Images", "Hammer" + str(i) + ".png"))
+    MOLARR_SWINGING_IMGS.append(
+        os.path.join("Game", "Assets", "Images", "Hammer" + str(i) + ".png"))
+
+MOLARR_SIZE = (91, 109)
+
+HAMMER_COLLISION_RECTS = [
+    {"size": (81, 60), "position": (85, 9)},
+    {"size": (78, 68), "position": (99, 14)},
+    {"size": (71, 71), "position": (113, 26)},
+    {"size": (64, 74), "position": (129, 37)},
+    {"size": (55, 75), "position": (138, 53)},
+    {"size": (45, 76), "position": (150, 67)},
+    {"size": (38, 76), "position": (156, 85)},
+    {"size": (45, 72), "position": (150, 101)}
+]
 
 MOLARR_IMG = os.path.join("Game", "Assets", "Images", "Mo'Larr.png")
 HAMMER_IMG = os.path.join("Game", "Assets", "Images", "Hammer.png")
